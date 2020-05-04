a5ea1d6e1ec8d140f2d0792eeb25394d34f28006
Surasak sincharoen <moomdate@gmail.com>
2020-05-04 21:43:52 +0700

    add

---
+++ test pangya/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace test_pangya
+{
+    partial class kheehee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.w_h = new System.Windows.Forms.NumericUpDown();
+            this.w_w = new System.Windows.Forms.NumericUpDown();
+            this.test = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.w_h)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.w_w)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Window width:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Window height";
+            // 
+            // w_h
+            // 
+            this.w_h.Location = new System.Drawing.Point(124, 74);
+            this.w_h.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.w_h.Name = "w_h";
+            this.w_h.Size = new System.Drawing.Size(120, 20);
+            this.w_h.TabIndex = 3;
+            this.w_h.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // w_w
+            // 
+            this.w_w.Location = new System.Drawing.Point(124, 45);
+            this.w_w.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.w_w.Name = "w_w";
+            this.w_w.Size = new System.Drawing.Size(120, 20);
+            this.w_w.TabIndex = 3;
+            this.w_w.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(262, 47);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(109, 47);
+            this.test.TabIndex = 4;
+            this.test.Text = "Resize";
+            this.test.UseVisualStyleBackColor = true;
+            this.test.Click += new System.EventHandler(this.Test_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(572, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(526, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "degree";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(526, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "radians:";
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(572, 93);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "label4";
+            this.label7.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(550, 117);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "a:";
+            this.label8.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(550, 142);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "b:";
+            this.label9.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(572, 117);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "label4";
+            this.label10.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(572, 142);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 13);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "label4";
+            this.label11.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // kheehee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 921);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.w_w);
+            this.Controls.Add(this.w_h);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "kheehee";
+            this.Text = "kheehee";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            ((System.ComponentModel.ISupportInitialize)(this.w_h)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.w_w)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown w_h;
+        private System.Windows.Forms.NumericUpDown w_w;
+        private System.Windows.Forms.Button test;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ test pangya/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_pangya
+{
+    public partial class kheehee : Form
+    {
+        int mouse_x;
+        int mouse_y;
+        public kheehee()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+           // this.mouse_x = e.X;
+           //  this.mouse_y = e.Y;
+           //  label1.Text = "X:" + e.X + " Y:"+  e.Y;
+
+            label4.Text = this.getDeegree().ToString();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+        private double getDeegree()
+        {
+            double result;
+            if (this.mouse_y == 848)
+            {
+                return 90.0;
+            }
+            else
+            {
+                double a = Math.Abs(this.mouse_x - 1182);
+                double b = Math.Abs(848 - this.mouse_y);
+                label10.Text = a.ToString();
+                label11.Text = b.ToString();
+
+                double radians = Math.Atan(a /b);
+                label7.Text = radians.ToString();
+                result = (180.0 / Math.PI) * radians;
+                return result;
+            }
+          
+        }
+        private void Test_Click(object sender, EventArgs e)
+        {
+            this.Size = new Size((int)w_w.Value,(int)w_h.Value);
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.mouse_x = e.X;
+            this.mouse_y = e.Y;
+            label1.Text = "X:" + e.X + " Y:" + e.Y;
+
+            label4.Text = this.getDeegree().ToString();
+        }
+
+        private void Label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
